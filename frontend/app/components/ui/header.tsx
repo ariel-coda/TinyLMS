8de5f50fd167e4c395deb75619ad43bbbf8c173d
CodeGhost237 <arielbanaken@gmail.com>
2025-09-27 09:30:53 +0100

    changements UX

--- frontend/app/components/ui/header.tsx
+++ frontend/app/components/ui/header.tsx
@@ -134,9 +134,9 @@ const Header: React.FC<HeaderProps> = ({ scrollToReservation }) => {
                   <div className="w-8 h-8 bg-blue-600 rounded-lg flex items-center justify-center mr-3">
                     <Bird className="w-5 h-5 text-white" />
                   </div>
-                  <span className="text-2xl font-bold text-gray-900">
+                  <h1 className="text-xl font-bold text-blue-600">
                     tinyLMS
-                  </span>
+                  </h1>
                 </Link>
               </div>
             </div>
